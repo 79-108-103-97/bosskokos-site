a87d6c9be50b23db664a78ccf592e15879b59e4b
Semenova Olga <82965608+79-108-103-97@users.noreply.github.com>
2022-01-21 08:56:10 +1100

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'host'  => 'localhost',
-    'dbname' => 'cr75599_proba',
-    'user'   => 'cr75599_proba',
-    'password' => 'cs034fe07822G',
+    'dbname' => 'dbname',
+    'user'   => 'user',
+    'password' => 'password',
 );
